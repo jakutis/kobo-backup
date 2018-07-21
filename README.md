b8361dce5cba97b2cb8dc148dbb5980b27c82c97
Vytautas Jakutis <vytautas@jakut.is>
2018-07-21 20:13:00 +0300

    release 1.0.0

---
+++ README.md
@@ -0,0 +1,8 @@
+# kobo-backup
+
+Backup your Kobo device bookmarks, highlights, etc.
+
+## Usage
+
+1. `npm install -g kobo-backup`
+2. `kobo-backup /path/to/kobo/mount/directory/with/epub/files /path/to/outputdirectory`
---
+++ index.js
@@ -0,0 +1,24 @@
+var sqlite3 = require('sqlite3');
+var path = require('path');
+var fs = require('fs');
+
+var koboDirectory = process.argv[2];
+var outputDirectory = process.argv[3];
+var db = new sqlite3.Database(path.join(koboDirectory, '.kobo', 'KoboReader.sqlite'));
+
+db.all('SELECT VolumeID, DateCreated, Text FROM Bookmark ORDER BY VolumeID', function (err, rows) {
+  if (err) {
+    console.log(err.stack);
+    process.exit(1);
+    return;
+  }
+  var filename = path.join(outputDirectory, 'bookmarks.json');
+  var contents = JSON.stringify(rows, null, 2);
+  fs.writeFile(filename, contents, function (err) {
+    if (err) {
+      console.log(err.stack);
+      process.exit(1);
+      return;
+    }
+  });
+});

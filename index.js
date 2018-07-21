bd81cd2e8a37a1be410a98a8dda02c01504f55f1
Vytautas Jakutis <vytautas@jakut.is>
2018-07-21 20:28:41 +0300

    change output format - do not leak the kobo domain objects

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ var fs = require('fs');
 var koboDirectory = process.argv[2];
 var outputDirectory = process.argv[3];
 var db = new sqlite3.Database(path.join(koboDirectory, '.kobo', 'KoboReader.sqlite'));
+var getFileNameFromVolumeId = function (volumeId) {
+  var prefix = 'file:///mnt/onboard/';
+  if (volumeId.substr(0, prefix.length) !== prefix) {
+    throw new Error('Volume ID "' + volumeId + '" is not prefixed with "' + prefix + '"');
+  }
+  return volumeId.substr(prefix.length);
+};
+var getDateStringFromDateCreated = function (dateCreated) {
+  dateCreated += 'Z';
+  if (isNaN(Date.parse(dateCreated))) {
+    throw new Error('DateCreated "' + dateCreated + '" cannot be parsed');
+  }
+  return dateCreated;
+}
 
 db.all('SELECT VolumeID, DateCreated, Text FROM Bookmark ORDER BY VolumeID', function (err, rows) {
   if (err) {
@@ -14,8 +28,13 @@ db.all('SELECT VolumeID, DateCreated, Text FROM Bookmark ORDER BY VolumeID', fun
     process.exit(1);
     return;
   }
-  var filename = path.join(outputDirectory, 'bookmarks.json');
+  rows = rows.map(r => ({
+    createdAt: new Date(getDateStringFromDateCreated(r.DateCreated)),
+    filename: getFileNameFromVolumeId(r.VolumeID),
+    text: r.Text
+  }));
   var contents = JSON.stringify(rows, null, 2);
+  var filename = path.join(outputDirectory, 'bookmarks.json');
   fs.writeFile(filename, contents, function (err) {
     if (err) {
       console.log(err.stack);
